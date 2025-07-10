f82c4499727bf9195ba42c4e2702e5e6c55ea223
tito69-hola <he.benavente@duocuc.cl>
2025-07-10 15:46:48 -0400

    Add files via upload

---
+++ funciones.py
@@ -0,0 +1,8 @@
+import main
+
+menu = """*** MENU PRINCIPAL ***
+1. Stock marca.
+2. Búsqueda por precio.
+3. Listado de productos.
+4. Salir."""
+
---
+++ main.py
@@ -0,0 +1,12 @@
+import funciones
+
+menu = """*** MENU PRINCIPAL ***
+1. Stock marca.
+2. Búsqueda por precio.
+3. Listado de productos.
+4. Salir."""
+
+while True:
+    print(menu)
+    input("seleccione una opcion: ")
+    break
